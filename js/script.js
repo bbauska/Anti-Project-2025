026e4836d7d10c0999b0939f89e6c04169f0a3cc
Brian Bauska <brianbauska@gmail.com>
2026-09-23 21:09:45 -0700

    Implement menu toggle functionality in script.js
    Added menu open/close functionality and initial hiding of elements.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-[...document.querySelectorAll('li')].forEach(li => 
-  li.addEventListener('click', e => (e.stopPropagation(), li.classList.toggle('done')))
-);
+/* /js/script.js of Anti-Project-2025 for anti-project-2025.bauska.org */
+/* hide these elements on page load */
+$('#main-menu, .menu-close').hide();
+
+/* open menu on menu open click */
+$('.menu-open').on('click', function(e) {
+  e.preventDefault();
+  $('#main-menu').slideDown();
+  $('.menu-open').hide();
+  $('.menu-close').show();
+});
+
+/* close menu on menu close or link click */
+$('.menu-close').on('click', function(e) {
+  e.preventDefault();
+  $('#main-menu').slideUp();
+  $('.menu-open').show();
+  $('.menu-close').hide();
+});
+
+/* close menu on menu close or link click */
+$('#main-menu a').on('click', function() {
+  $('#main-menu').slideUp();
+  $('.menu-open').show();
+  $('.menu-close').hide();
+});
